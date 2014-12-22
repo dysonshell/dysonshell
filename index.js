4539a85556cf281e259e8d339a1be55a0246a521
Anson Yu Wang <opensource@undozen.com>
2014-12-22 10:22:45 +0800

    move express to peerDependencies & use express-state

--- index.js
+++ index.js
@@ -3,4 +3,5 @@ var express = require('express');
 var app, exports;
 app = exports = module.exports = express();
 
-require('express-expose')(app);
+require('express-state').extend(app);
+app.set('state namespace', 'MY_APP');
