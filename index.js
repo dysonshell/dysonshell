8cd19422e83da478426154d6d362703a7293ec6f
Anson Yu Wang <opensource@undozen.com>
2014-12-26 08:09:07 +0800

    rewrite, expose createApp() and createSubApp()

--- index.js
+++ index.js
@@ -1,8 +1,19 @@
 'use strict';
+var path = require('path');
 var express = require('express');
-var app, exports;
-app = exports = module.exports = express();
+var expstate = require('express-state');
 
-require('express-state').extend(app);
-app.set('state namespace', 'MY_APP');
-app.express = express;
+exports.createApp = function (appRoot) {
+    var app = express();
+    expstate.extend(app);
+    app.set('state namespace', 'MY_APP');
+    app.set('views', path.join(appRoot, 'views'));
+    return app;
+};
+
+exports.createSubApp = function (subAppRoot) {
+    var subApp = express();
+    subApp.on('mount', function (app) {
+        subApp.set('views', [path.join(subAppRoot, 'views')].concat(app.get('views')));
+    });
+};
