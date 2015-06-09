652c145432c01b9ca6fd48632d0253ac4ea294d7
Anson Yu Wang <opensource@undozen.com>
2015-06-09 13:45:48 +0800

    fix bug

--- apiproxy.js
+++ apiproxy.js
@@ -1,13 +1,13 @@
 'use strict';
 var httpProxy = require('http-proxy');
 
-module.exports = function expose(app, urlBackend) {
+module.exports = function expose(app, urlBackend, namespace) {
     if (typeof urlBackend !== 'string') {
         throw new Error('urlBackend must be string');
     }
     app.set('state namespace', (namespace || 'CC'));
     var proxyApi = httpProxy.createProxyServer({
-        target: urlBackend.replace(/\/$/, '') + '/api';
+        target: urlBackend.replace(/\/$/, '') + '/api'
     });
     // 代理到 api 服务器
     app.use('/api', function (req, res, next) {
