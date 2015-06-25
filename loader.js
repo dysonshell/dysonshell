d88fc60af9480da6e097ace4128c9f26a60323ff
Anson Yu Wang <opensource@undozen.com>
2015-06-25 17:47:21 +0800

    conflict check not done

--- loader.js
+++ loader.js
@@ -5,39 +5,57 @@ var cccglob = require('@ds/cccglob');
 var log = require('bunyan-hub-logger')({app: 'web', name: 'loader'});
 var express = require('express');
 var hooker = require('./hooker');
+var methods = require('methods');
+var env = process.env.NODE_ENV || 'development';
+var isUat = process.env.NODE_APP_INSTANCE === 'uat';
+
+function createRouter(routerModule) {
+    var router = express.Router();
+    router.use = function () {
+        throw new Error('因为 ccc/*/router.js 的加载顺序不定，不能在这里使用 router.use() 方法，请在 web/index.js 的 dsBase.load() 之前添加全局 middleware');
+    };
+    methods.concat('all').forEach(function(m){
+        if (env === 'production' && !isUat) {
+            return router[m];
+        }
+        router[m] = (function(origMethod) {
+            return function () {
+                origMethod.call(this, function (req, res, next) {
+
+                    // check router conflicts
+                    if (req.routerFactoryModule && routerModule.filename !== req.routerFactoryModule.filename) {
+                        return next(new Error(
+                            '为避免冲突不能在不同的模块里处理相同的 url。冲突的 module：' +
+                            req.routerFactoryModule.filename + ' vs ' + routerModule.filename));
+                    }
+                    req.routerFactoryModule = routerModule;
+                    next();
+                });
+                return origMethod.apply(this, arguments);
+            };
+        }(router[m]));
+    });
+    return router;
+}
 
 module.exports = function load(app) {
     cccglob.sync('ccc/*/router.js').forEach(function (routerName) {
         var routerPath = require.resolve(routerName);
         log.trace('loading router ' + routerPath);
         var routerFactory = require(routerName);
-        if (routerFactory.loaded) {
-            return;
-        }
         var routerModule = require.cache[routerPath];
-        var router = express();
-        router.factoryModule = routerModule;
-        // TODO: check router conflicts
-        /*
-        router.use(function (req, res, next) {
-            if (req.routerFactoryModule) {
-                return next(new Error('为避免冲突不能在不同的模块里处理相同的 url。冲突的 module：' +
-                    req.routerFactoryModule.filename + ' vs ' + routerPath));
-            }
-            req.routerFactoryModule = routerModule;
-            next();
-        });
-        */
+        var router = createRouter(routerModule);
         routerFactory(router);
         var hookPath = path.join(path.dirname(routerPath), 'hook.js');
+        app.use(router);
         if (fs.existsSync(hookPath)) {
             log.trace('loading hook ' + hookPath);
             var hook = require(hookPath);
-            router.factoryModule = require.cache[hookPath];
+            var router = createRouter(require.cache[hookPath]);
             hooker(hook, router);
+            app.use(router);
             hook.loaded = true;
         }
-        app.use(router);
     });
     cccglob.sync('ccc/*/hook.js').forEach(function (hookName) {
         var hookPath = require.resolve(hookName);
