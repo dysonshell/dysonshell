c8b76c1d1cc5171cbd313f546d3b7db35830881a
Anson Yu Wang <opensource@undozen.com>
2015-08-24 09:46:08 +0800

    allow dot started path

--- loader.js
+++ loader.js
@@ -45,17 +45,29 @@ function createRouter(routerModule) {
         throw new Error('因为 ccc/*/router.js 的加载顺序不定，不能在这里使用 router.use() 方法，请在 web/index.js 的 dsBase.load() 之前添加全局 middleware');
     };
     methods.concat('all').forEach(function(m){
-        if (env === 'production' && !isUat) {
-            return router[m];
-        }
         router[m] = (function(origMethod) {
             return function (routePath) {
+                if (typeof routePath !== 'string') {
+                    throw new Error('router/hook path 必须为字符串');
+                }
+                var args = [].slice.apply(arguments);
+                var dreg = /^(\/*)\./;
+                var startedWithDot = routePath.match(dreg);
+                routePath = routePath.replace(dreg, '$1');
+                if (env === 'production' && !isUat) {
+                    // 生产环境不限制前缀（假设开发和测试已经保证没问题）
+                    // 并且不显示 /. 开头的路径
+                    if (!startedWithDot) {
+                        origMethod.apply(this, args);
+                    }
+                    return;
+                }
                 if ((routePath instanceof RegExp && !(prefix + '/').match(routePath)) || // RegExp 默认是匹配 /xxx/* 的
                     (typeof routePath === 'string' && (!(routePath.indexOf(prefix) === 0 && // 匹配 /xxx 与 /xxx/*
                         (!routePath[prefix.length] || routePath[prefix.length] === '/'))))) {
                     throw new Error('只能在 router.js 和 hook.js 里设置对应该模块的路径，如 ccc/login 只能设置 /login 或 /login/* 路径，而 ccc/account__payment 只能设置 /account/payment 或 /account/payment/* （双下划线转换成 /）');
                 }
-                return origMethod.apply(this, arguments);
+                return origMethod.apply(this, args);
             };
         }(router[m]));
     });
