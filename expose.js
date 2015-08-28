4ca3c62fd942a7d86089eb1811b967bd9de5cba6
Anson Yu Wang <opensource@undozen.com>
2015-08-28 09:45:12 +0800

    expose NODE_ENV & NODE_APP_INSTANCE

--- expose.js
+++ expose.js
@@ -4,4 +4,9 @@ var expState = require('express-state');
 module.exports = function expose(app, namespace) {
     expState.extend(app);
     app.set('state namespace', (namespace || 'CC'));
-};
+    app.use(function (req, res, next) {
+        res.expose(process.env.NODE_ENV, 'process.env.NODE_ENV');
+        res.expose(process.env.NODE_APP_INSTANCE, 'process.env.NODE_APP_INSTANCE');
+        next();
+    });
+e;
