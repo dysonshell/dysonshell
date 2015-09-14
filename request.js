a8a065dbead0eb12fa25bf5c6bda653a891670dc
Anson Yu Wang <opensource@undozen.com>
2015-09-14 17:52:07 +0800

    express-req-uest -> promisingagent

--- request.js
+++ request.js
@@ -1,15 +1,27 @@
 'use strict';
+require('http').globalAgent.maxSockets = Infinity
+require('https').globalAgent.maxSockets = Infinity
+var proagent = require('promisingagent');
 
 module.exports = function expose(app, urlBackend) {
-    require('express-req-uest').augmentReqProto(app.request, {
-        prefix: urlBackend,
-        augments: {
-            cookies: false,
-            custom: function (r, req) {
-                if (req.cookies.ccat) {
-                    r.set('Authorization', 'Bearer ' + req.cookies.ccat);
-                }
-            }
+    app.use(require('cookie-parser')());
+    app.use(function (req, res, next) {
+        var ips = [];
+        if (req.ip && req.ip.indexOf('127.0.0.1') === -1) ips.push(req.ip);
+        if (req.ips && Array.isArray(req.ips)) {
+          ips = ips.concat(req.ips[0] == req.ip ? req.ips.slice(1) : req.ips);
         }
+        var headers = {};
+        headers['X-Forwarded-For'] = ips.join(', ');
+        if (req.cookies.ccat) {
+            headers.Authorization = 'Bearer ' + req.cookies.ccat;
+        }
+        if (req.header('Authorization')) {
+            headers.Authorization = req.header('Authorization');
+        }
+        req.uest = proagent.extend(urlBackend.replace(/\/+$/, ''), {
+            headers: headers,
+        });
+        next();
     });
 };
