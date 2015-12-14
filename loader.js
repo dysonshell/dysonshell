38fa373e291d020c354b17d875dd5ae2ba3eb04e
Anson Yu Wang <opensource@undozen.com>
2015-12-15 05:23:18 +0800

    fix routerName -> routerPrefix & /index

--- loader.js
+++ loader.js
@@ -13,9 +13,10 @@ var DSC = config.dsComponentPrefix || 'dsc';
 var DSCns = DSC.replace(/^\/+/, '').replace(/\/+$/, '');
 DSC = DSCns + '/';
 
-module.exports = function load(routerName) {
+module.exports = function load(routerPrefix) {
     var exportRouter = express.Router();
-    dsGlob.sync(DSC + '*/routers/'+routerName+'.js').forEach(function (routerName) {
+    dsGlob.sync(DSC + '*/routers/'+routerPrefix+'.js').forEach(function (routerName) {
+        console.log(routerName);
         var routerPath = require.resolve(routerName);
         log.trace('loading router ' + routerPath);
         var routerFactory = require(routerName);
@@ -26,6 +27,9 @@ module.exports = function load(routerName) {
         routerFactory(router);
         router.use(removeRouterFactory());
         var prefix = '/' + getComponentName(routerPath);
+        if (prefix === '/index') {
+            exportRouter.use(router);
+        }
         exportRouter.use(prefix, router);
     });
     return exportRouter;
