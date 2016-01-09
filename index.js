6cb15fcda77993937c5dfb3fcb5854bf697e1c95
Anson Yu Wang <opensource@undozen.com>
2016-01-09 15:25:02 +0800

    delay ds-pack/build require

--- index.js
+++ index.js
@@ -4,5 +4,9 @@ exports.loader = require('./loader');
 exports.expose = require('./expose');
 exports.apiproxy = require('./apiproxy');
 exports.request = require('./request');
-exports.build = require('ds-pack/build');
+Object.defineProperty(exports, 'build', {
+    get: function () {
+         return require('ds-pack/build');
+    },
+});
 exports.watchify = require('ds-pack/augment-app');
